004484832e3a99d50a056d1121dbc871db8b2038
Srikant Patnaik <srikant@gnu-linux.org>
2015-08-11 10:45:02 +0000

    added working program, run using sudo

---
+++ 19-python-learn-through-coding/19.2-turn-on-LED-if-someone-logged-in-remotely.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import subprocess
+import RPi.GPIO as GPIO
+import time
+
+LED=21
+
+# Use BCM style of pin numbering
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(LED, GPIO.OUT)
+
+def check_ssh():
+    # Run a netstat command and check whether ssh connection available
+    netstat = subprocess.Popen('netstat', stdout=subprocess.PIPE)
+    output = subprocess.Popen(('grep', 'ssh'), stdin=netstat.stdout, stdout=subprocess.PIPE)
+    output.communicate()
+    return output.returncode
+
+
+while True:
+    time.sleep(1)
+    if check_ssh():
+        GPIO.output(LED, False)
+    else:
+   	GPIO.output(LED, True)
+

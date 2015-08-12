ce4ed1cbf2d481ccf7f24571b44e0d7efd0d13e1
Srikant Patnaik <srikant@gnu-linux.org>
2015-08-12 10:19:26 +0530

    added void setup, void loop

--- 19-python-learn-through-coding/19.3/19.3-push-button-download-weather-from-inet.py
+++ 19-python-learn-through-coding/19.3/19.3-push-button-download-weather-from-inet.py
@@ -4,18 +4,19 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 import yweather
-location="New York, USA"  # Change for yours
 
+location="New York, USA"  # Change for yours
 # Push button connected to GPIO 21
 button=21
 
-GPIO.setwarnings(False)
-# Clean/Release any previous GPIO access
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-# Make button input, and enable software PULL UP(means, internally
-# the pin is configured as HIGH)
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+def setup():
+	GPIO.setwarnings(False)
+	# Clean/Release any previous GPIO access
+	GPIO.cleanup()
+	GPIO.setmode(GPIO.BCM)
+	# Make button input, and enable software PULL UP(means, internally
+	# the pin is configured as HIGH)
+	GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
 def fetch_weather():
 	client = yweather.Client()
@@ -24,9 +25,12 @@ def fetch_weather():
 	return 'Humidity: ' + my_weather["atmosphere"]["humidity"] \
 		   + '%, Temp: ' + my_weather["condition"]["temp"] + 'F'
 
-while True:
-	GPIO.wait_for_edge(button, GPIO.FALLING)
-	osd = "echo -n " + fetch_weather() + ', ' + location + "| osd_cat --color=red --pos=top --align=right --offset=50 --indent=50 --delay 2  --font '-*-fixed-*-*-*-*-35-*-*-*-*-*-*-*' "
-	subprocess.Popen(osd, shell=True)
-	time.sleep(1) 
+def loop():
+	while True:
+		GPIO.wait_for_edge(button, GPIO.FALLING)
+		osd = "echo -n " + fetch_weather() + ', ' + location +\
+		 "| osd_cat --color=red --pos=top --align=right --offset=50\
+ 		--indent=50 --delay 2  --font '-*-fixed-*-*-*-*-35-*-*-*-*-*-*-*' "
+		subprocess.Popen(osd, shell=True)
+		time.sleep(1)
 

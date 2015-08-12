6d704eee30b39dfd8ff53b43c8baf44930493558
Srikant Patnaik <srikant@gnu-linux.org>
2015-08-12 05:17:20 +0000

    added setup/loop to 19.2

--- 19-python-learn-through-coding/19.2/19.2-turn-on-LED-if-someone-logged-in-remotely.py
+++ 19-python-learn-through-coding/19.2/19.2-turn-on-LED-if-someone-logged-in-remotely.py
@@ -2,14 +2,15 @@
 import subprocess
 import RPi.GPIO as GPIO
 import time
-# Just in case, unlock any blocked pins
-GPIO.cleanup()
 LED=21
 
-# Use BCM style of pin numbering
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(LED, GPIO.OUT)
+def setup():
+	# Just in case, unlock any blocked pins
+	GPIO.cleanup()
+	# Use BCM style of pin numbering
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(LED, GPIO.OUT)
 
 def check_ssh():
     # Run a netstat command and check whether ssh connection available
@@ -18,11 +19,15 @@ def check_ssh():
     output.communicate()
     return output.returncode
 
+def loop():
+	while True:
+	    time.sleep(1)
+	    if check_ssh():
+    	    GPIO.output(LED, False)
+	    else:
+	   	GPIO.output(LED, True)
 
-while True:
-    time.sleep(1)
-    if check_ssh():
-        GPIO.output(LED, False)
-    else:
-   	GPIO.output(LED, True)
+if __name__ == "__main__":
+    setup()
+    loop()
 
